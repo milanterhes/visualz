b86d4816fa12fe78efe5811574919eedd89b24b3
Milan Terhes <terhes.milan@gmail.com>
2024-07-27 16:08:16 +0200

    i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()],
+  i18n: {
+    defaultLocale: "hu",
+    locales: ["hu", "de", "en"],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
