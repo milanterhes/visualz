22afb694e98670d7152cad0253065a464df17a68
Milan Terhes <terhes.milan@gmail.com>
2024-08-14 18:36:47 +0200

    fix language picker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind()],
   i18n: {
     defaultLocale: "hu",
     locales: ["hu", "de", "en"],
     routing: {
-      prefixDefaultLocale: false
-    }
-  }
+      prefixDefaultLocale: false,
+    },
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'sans': ['Space Grotesk Variable', "sans-serif"],
-				'serif': ['Space Grotesk Variable', "sans-serif"],
-				'mono': ['Space Grotesk Variable', "sans-serif"],
-			  },
-		},
-	},
-	plugins: [],
-	darkMode: "class"
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Space Grotesk Variable", "sans-serif"],
+        serif: ["Space Grotesk Variable", "sans-serif"],
+        mono: ["Space Grotesk Variable", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  darkMode: "class",
+};
